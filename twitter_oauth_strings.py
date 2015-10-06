f578346b26d815dcf4a1483f46566563f07229a4
Clinton Dow <cdow@esri.com>
2015-10-06 12:49:24 -0700

    Added new python toolbox for recording tweets to a local gdb

---
+++ twitter_oauth_strings.py
@@ -0,0 +1,5 @@
+cons_tok = "5t3DLzDcI8eCrMcGCHA94hhIG"
+cons_sec = "zyzse9kfWQCJv0GnZHTFvnv3ESrOoaEFNtTNaSe8zj5B3RJZHT"
+
+app_tok = "3427938612-dsed9lZaqnelYVF8x5Vv2AzSZ0f4j605tqXGgNK"
+app_sec = "hrEdjFSQiFlWdyUyHJ2fV9p8YKx6NKNY3ex8BIEfJtLaN"
